e401337832015f2aa95aa97fb78116dae4b78873
NUGROHOADI11 <deeforce112002@gmail.com>
2023-12-13 17:18:32 +0700

    photo profile

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,188 +3,206 @@ const model = require("../config/models/index");
 const uuid = require("uuid");
 
 const HTTP_STATUS = {
-  OK: 200,
-  CREATED: 201,
-  NOT_FOUND: 404,
-  UNAUTHORIZED: 401,
-  BAD_REQUEST: 400,
-  INTERNAL_SERVER_ERROR: 500,
+    OK: 200,
+    CREATED: 201,
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401,
+    BAD_REQUEST: 400,
+    INTERNAL_SERVER_ERROR: 500,
 };
 
 const controller = {};
 
 controller.getAll = async function (req, res) {
-  try {
-      const products = await model.User.findAll();
+    try {
+        const user = await model.User.findAll();
 
-      res.json({ success: true, data: products });
-  } catch (e) {
-      console.error(e);
-      res.status(500).json({
-          success: false,
-          message: "Internal server error",
-      });
-  }
+        res.json({ success: true, data: user });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
 };
 
-
 controller.getOne = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await model.User.findByPk(userId);
+    try {
+        const { userId } = req.params;
+        const user = await model.User.findByPk(userId);
 
-    if (!user) {
-      return res.status(HTTP_STATUS.NOT_FOUND).json({ success: false, message: "User not found" });
+        if (!user) {
+            return res
+                .status(HTTP_STATUS.NOT_FOUND)
+                .json({ success: false, message: "User not found" });
+        }
+
+        res.json({
+            success: true,
+            data: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                FullName: user.FullName,
+                Address: user.Address,
+                Phone: user.Phone,
+                Image: user.ProfilePhotoURL,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Internal server error",
+        });
     }
-
-    res.json({ success: true, data: user });
-  } catch (error) {
-    console.error(error);
-    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: "Internal server error",
-    });
-  }
 };
 
 controller.login = async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  try {
-    const user = await model.User.findOne({ where: { username } });
+    try {
+        const user = await model.User.findOne({ where: { username } });
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(HTTP_STATUS.UNAUTHORIZED).json({
-        error: "Invalid username or password",
-      });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+                error: "Invalid username or password",
+            });
+        }
+
+        const token = uuid.v4();
+        user.Token = token;
+
+        res.status(HTTP_STATUS.OK).json({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            token,
+        });
+
+        await user.save();
+    } catch (error) {
+        console.error(error);
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            error: "Internal server error",
+        });
     }
-
-    const token = uuid.v4();
-    user.Token = token;
-
-    res.status(HTTP_STATUS.OK).json({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      token,
-    });
-
-    await user.save();
-  } catch (error) {
-    console.error(error);
-    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-      error: "Internal server error",
-    });
-  }
 };
 
 controller.register = async (req, res) => {
-  try {
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    try {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const user = await model.User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
+        const user = await model.User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
 
-    res.status(HTTP_STATUS.CREATED).json({
-      message: "Create user successful",
-      id: user.id,
-      username: user.username,
-      email: user.email,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(HTTP_STATUS.NOT_FOUND).json({
-      data: e.message,
-    });
-  }
+        res.status(HTTP_STATUS.CREATED).json({
+            message: "Create user successful",
+            data: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+            },
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(HTTP_STATUS.NOT_FOUND).json({
+            data: e.message,
+        });
+    }
 };
 
-
 controller.logout = async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const user = await model.User.findByPk(userId);
+    const { userId } = req.params;
+    try {
+        const user = await model.User.findByPk(userId);
 
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        user.Token = null;
+        await user.save();
+
+        res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    user.Token = null;
-    await user.save();
-
-    res.status(200).json({ message: "Logout successful" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
 };
 
 controller.editDataUser = async (req, res) => {
-  const { userId } = req.params;
-  const { username, FullName, Address, Phone } = req.body;
+    const { userId } = req.params;
+    const { FullName, Address, Phone } = req.body;
+    var imageUrl = "";
 
-  try {
-    const user = await model.User.findByPk(userId);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
+    if (req.file && req.file.cloudStoragePublicUrl) {
+        imageUrl = req.file.cloudStoragePublicUrl;
     }
+    try {
+        const user = await model.User.findByPk(userId);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: "User not found" });
+        }
 
-    user.username = username || user.username;
-    user.FullName = FullName || user.FullName;
-    user.Address = Address || user.Address;
-    user.Phone = Phone || user.Phone;
+        user.FullName = FullName || user.FullName;
+        user.Address = Address || user.Address;
+        user.Phone = Phone || user.Phone;
+        user.ProfilePhotoURL = imageUrl || user.ProfilePhotoURL;
 
-    await user.save();
+        await user.save();
 
-    res.json({
-      success: true,
-      message: "User updated successfully",
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      FullName: user.FullName,
-      Address: user.Address,
-      Phone: user.Phone
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-    });
-  }
+        res.json({
+            success: true,
+            message: "User updated successfully",
+            data: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                FullName: user.FullName,
+                Address: user.Address,
+                Phone: user.Phone,
+                Image: user.ProfilePhotoURL,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
 };
 
 controller.delete = async function deleteUser(req, res) {
-  const { userId } = req.params;
+    const { userId } = req.params;
 
-  try {
-      const user = await model.User.findByPk(userId);
-      if (!user) {
-          return res
-              .status(404)
-              .json({ success: false, message: "User not found" });
-      }
+    try {
+        const user = await model.User.findByPk(userId);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: "User not found" });
+        }
 
-      // await model.Credential.destroy({ where: { UserId: userId } });
-      // await model.Product.destroy({ where: { RenterId: userId } });
-      await user.destroy();
+        await user.destroy();
 
-      res.json({ success: true, message: "User deleted successfully" });
-  } catch (error) {
-      console.error(error);
-      res.status(500).json({
-          success: false,
-          message: "Internal server error",
-      });
-  }
+        res.json({ success: true, message: "User deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
 };
 
 module.exports = controller;
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const db = require("../config/database/db");
 const controller = require("../controllers/index");
 const authMiddleware = require("../middleware/auth");
+const Multer = require("multer");
+const imgUpload = require("../middleware/upload_gcp");
+
+const multer = Multer({
+    storage: Multer.MemoryStorage,
+    fileSize: 5 * 1024 * 1024,
+});
 
 // router.use(authMiddleware);
 router.get("/:userId", authMiddleware, controller.user.getOne);
@@ -10,7 +17,13 @@ router.get("/", controller.user.getAll);
 router.post("/register", controller.user.register);
 router.post("/login", controller.user.login);
 router.post("/:userId/logout", authMiddleware, controller.user.logout);
-router.patch("/:userId", authMiddleware, controller.user.editDataUser);
-router.delete("/:userId",authMiddleware, controller.user.delete);
+router.patch(
+    "/:userId",
+    authMiddleware,
+    multer.single("uploadimage"),
+    imgUpload.uploadToGcs,
+    controller.user.editDataUser
+);
+router.delete("/:userId", authMiddleware, controller.user.delete);
 
 module.exports = router;
